d0a801160795666b619ec6a24de4589a4ac301d2
diakurnia <diakurnia20@gmail.com>
2020-02-25 21:13:43 +0700

    modified routes

--- routes/PelangganRoutes.js
+++ routes/PelangganRoutes.js
@@ -4,7 +4,14 @@ const isAuthenticated = require('../middleware/checkAuth');
 
 const router = Router();
 
+//route without token
+router.get('/', PelangganController.getAllPelanggan);
+router.post('/', PelangganController.addPelanggan);
+router.get('/:id', PelangganController.getPelangganById);
+router.put('/:id', PelangganController.updatePelanggan);
+router.delete('/:id', PelangganController.deletePelanggan);
 
+//route with token to authorization
 router.get('/', isAuthenticated, PelangganController.getAllPelanggan);
 router.post('/', isAuthenticated, PelangganController.addPelanggan);
 router.get('/:id', isAuthenticated, PelangganController.getPelangganById);
--- routes/PesananRoutes.js
+++ routes/PesananRoutes.js
@@ -4,7 +4,15 @@ const isAuthenticated = require('../middleware/checkAuth');
 
 const router = Router();
 
+//route pesanan without authorization
+router.get('/', PesananController.getAllPesanan);
+router.post('/', PesananController.addPesanan);
+router.get('/:id', PesananController.getPesananById);
+router.get('/:id/pelanggan', PesananController.getPesananOfPelanggan);
+router.put('/:id', PesananController.updatePesanan);
+router.delete('/:id', PesananController.deletePesanan);
 
+//route with token to authorization
 router.get('/', isAuthenticated, PesananController.getAllPesanan);
 router.post('/', isAuthenticated, PesananController.addPesanan);
 router.get('/:id', isAuthenticated, PesananController.getPesananById);
